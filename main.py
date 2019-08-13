79b251456cfc03d706c15e66f495ebc8addb582c
Александр Тен <orionapplepie@gmail.com>
2019-08-13 21:54:57 +1000

    improved fetch functions for downloading images

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-import argparse
 import os
 import glob
 
+from instabot import Bot
 import requests
 from requests.compat import urlparse, urljoin
-from instabot import Bot
 
 
 IMAGES_FOLDER = "images"
-BASE_SPACEX_API_URL = "https://api.spacexdata.com/v3/"
+SPACEX_API_URL = "https://api.spacexdata.com/v3/"
 HUBBLE_API_IMAGE_URL = "http://hubblesite.org/api/v3/image/"
 HUBBLE_API_IMAGES_URL = "http://hubblesite.org/api/v3/images/"
 
 
 def download_image(url="", folder="", img_name=""):
-    """Function for downloading image by given url and save it to folder."""
+    """Function for downloading image by given url
+    and saving it to given folder."""
+    try:
+        os.makedirs(folder)
+    except FileExistsError:
+        pass
+    # print("Downloading {0}\n".format(url))
     response = requests.get(
         url=url, verify=False
     )
     response.raise_for_status()
-    # TODO: make names by enumerate
     file_name = os.path.join(folder, img_name)
+
     with open(file_name, 'wb') as file:
         file.write(response.content)
 
 
-def get_filename_by_link(link=""):
-    url_path = urlparse(link).path
-    filename = os.path.basename(url_path)
-    return filename
-
-
 def get_all_launches_with_images():
+    """Returns dict with all last and previous launches if it having images."""
     all_launches_url_path = "launches"
     all_launches_url = urljoin(
-        BASE_SPACEX_API_URL, all_launches_url_path
+        SPACEX_API_URL, all_launches_url_path
     )
     response = requests.get(
         url=all_launches_url
     )
     response.raise_for_status()
+
     launches_with_images = {}
     for launch in response.json():
         if launch['links']['flickr_images']:
-            launches_with_images[launch['flight_number']] = {
-                "launch_date": launch['launch_date_utc'],
-                "flickr_images": launch['links']['flickr_images'] 
-            }
+            launches_with_images[
+                launch['flight_number']
+            ] = launch['links']['flickr_images'] 
     return launches_with_images
 
 
@@ -54,34 +54,20 @@ def get_latest_launch_images_links():
     """Return structure with latest links of spacex launches."""
     latest_launch_url_path = "launches/latest"
     latest_launch_url = urljoin(
-        BASE_SPACEX_API_URL, latest_launch_url_path
+        SPACEX_API_URL, latest_launch_url_path
     )
     response = requests.get(
         url=latest_launch_url
     )
     response.raise_for_status()
+
     latest_launch_images_links = response.json()['links']['flickr_images']
     if not latest_launch_images_links:
         all_launches = get_all_launches_with_images()
-        latest_launch = all_launches[max(all_launches)]
-        latest_launch_images_links = latest_launch['flickr_images']
+        # get last number
+        latest_launch_images_links = all_launches[max(all_launches)]
 
     return latest_launch_images_links
-
-
-def fetch_spacex_last_launch():
-    try:
-        os.makedirs(IMAGES_FOLDER)
-    except FileExistsError:
-        print("folder exist, skipping...")
-
-    for link in get_latest_launch_images_links():
-        file_name = get_filename_by_link(link)
-        download_image(
-            url=link,
-            folder=IMAGES_FOLDER,
-            img_name=file_name
-        )
 
 
 def get_file_extension(link=""):
@@ -89,73 +75,85 @@ def get_file_extension(link=""):
     return parts[-1]
 
 
+def fetch_spacex_last_launch():
+    """Download all pics from last SpaceX launch."""
+    default_filename = "spacex"
+
+    for num, link in enumerate(get_latest_launch_images_links()):
+        file_ext = get_file_extension(link)
+        file_name = ".".join(
+            [default_filename + str(num), file_ext]
+        )
+        download_image(
+            url=link,
+            folder=IMAGES_FOLDER,
+            img_name=file_name
+        )
+
+
 def fetch_image_hubble(image_id=""):
+    """Download one pic by id from Hubble Site."""
     response = requests.get(
         url=urljoin(HUBBLE_API_IMAGE_URL, str(image_id))
     )
     response.raise_for_status()
     image_files = response.json()["image_files"]
+
     links = []
     for image_file in image_files:
         links.append(
             image_file["file_url"]
         )
+    # use last link - best quality image
     final_image_link = "".join(["https:", links[-1]])
-    print(final_image_link)
+
     file_name = ".".join(
         [
             str(image_id),
             get_file_extension(final_image_link)
         ]
     )
+
     download_image(
         url=final_image_link,
         folder=IMAGES_FOLDER,
         img_name=file_name
     )
 
-def get_hubble_collection_images_ids(collection_name=""):
-    """Fetch id's of collection."""
+
+def fetch_hubble_collection_images(collection_name=""):
+    """Fetch pics of collection from Hubble Site."""
     url = urljoin(HUBBLE_API_IMAGES_URL, collection_name)
-    print(url)
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    images_ids = []
     images = response.json()
+
     for image in images:
-        images_ids.append(image["id"])
-    return images_ids
+        fetch_image_hubble(image["id"])
 
 
 def main():
-    # fetch_spacex_last_launch()
-    # fetch_image_hubble(4001)
-    # ids = get_hubble_collection_images_ids("spacecraft")
-    # print(ids)
-    # for id in ids:
-    #     print("downloading {0}".format(id))
-    #     fetch_image_hubble(id)
-    parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument('-u', type=str, help="username")
-    parser.add_argument('-p', type=str, help="password")
-    parser.add_argument('-caption', type=str, help="caption for photo")
-    args = parser.parse_args()
+    # download pics of last SpaceX launch
+    fetch_spacex_last_launch()
 
-    bot = Bot()
-    bot.login()
-    user_id = bot.get_user_id_from_username("north_viking_saw")
-    user_info = bot.get_user_info(user_id)
-    print(user_info['biography'])
+    # download pics of collection from Hubble Site
+    fetch_hubble_collection_images(
+        "holiday_cards"
+    )
 
-    pics = glob.glob("./images/pics/*.jpg")
-    try:
-        for pic in pics:
-            print(pic)
-            bot.upload_photo(pic, caption="testing")
-            if bot.api.last_response.status_code != 200:
-                print(bot.api.last_response)
-    except Exception as e:
-        print(str(e))
+    # # Upload pics from image folder to Instagram
+    # bot = Bot()
+    # bot.login()
+    # pics = glob.glob("./images/*.jpg")
+
+    # try:
+    #     for pic in pics:
+    #         print(pic)
+    #         bot.upload_photo(pic, caption="testing")
+    #         if bot.api.last_response.status_code != 200:
+    #             print(bot.api.last_response)
+    # except Exception as e:
+    #     print(str(e))
 
 
 if __name__ == "__main__":
